4e83f3082dd6699ed5d1d809b2d9ef70436afee7
Winstonnub <cywinston.liang@mail.utoronto.ca>
2025-09-08 13:32:51 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz_original.java
+++ src/lab1/FizzBuzz_original.java
@@ -6,8 +6,8 @@ package lab1;
 class FizzBuzz {
 
     public static void main(String[] args) {
-        int i = 1;
-        while (i < 100) {
+
+        for (int i = 1; i < 100; i++) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
@@ -31,7 +31,6 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
-            i++;
         }
     }
 }
