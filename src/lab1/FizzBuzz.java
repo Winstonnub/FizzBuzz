6fea38a4da4718b8a81db8671221ad092f31a9e1
Winstonnub <cywinston.liang@mail.utoronto.ca>
2025-09-08 14:51:15 -0400

    Task 4.1

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -4,8 +4,7 @@ package lab1;
  * Solve the lab1.FizzBuzz challenge.
  */
 class FizzBuzz {
-
-    public static void main() {
+    public static void main(String[] args) {
         
         int i = 1;
         while (i < 100) {
@@ -36,8 +35,4 @@ class FizzBuzz {
 
             System.out.println(i);
 
-        }
-        i++;
-        return i;
-    }
-}
+        
