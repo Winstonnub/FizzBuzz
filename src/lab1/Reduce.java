05a1797b570d5208316837ebc47e36641fce3720
Winstonnub <cywinston.liang@mail.utoronto.ca>
2025-09-08 22:41:24 -0400

    Completed Task 4.2

---
+++ src/lab1/Reduce.java
@@ -0,0 +1,23 @@
+package lab1;
+
+public class Reduce {
+    public static void main(String[] args) {
+        System.out.println(reduce(100));
+
+    }
+
+    public static int reduce(int n) {
+        int count = 0;
+        while (n != 0){
+            if (n % 2 == 0){
+                n /= 2;
+                count++;
+            }
+            else{
+                n -= 1;
+                count++;
+            }
+        }
+        return count;
+    }
+}
